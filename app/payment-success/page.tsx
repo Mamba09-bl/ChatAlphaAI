76b5a328633629cc7ee409f6d95ebe2817806ef9
unknown <hamza.ahmed.abbasi07@gmail.com>
2025-12-09 03:54:23 +0500

    upload full project

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -1,6 +1,11 @@
+// app/payment-success/page.tsx
+"use client";
+
 import dynamic from "next/dynamic";
 
-const PaymentSuccessClient = dynamic(() => import("./client"), { ssr: false });
+const PaymentSuccessClient = dynamic(() => import("./client"), {
+  ssr: false,
+});
 
 export default function Page() {
   return <PaymentSuccessClient />;
